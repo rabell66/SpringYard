f06d92d92f3f58791d48fb00fef21dad50b896fc
rabell66 <raabell66@gmail.com>
2017-08-05 07:01:42 -0500

    setting up controller

---
+++ src/main/java/com/example/customer/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.example.customer.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(path ="/", method = RequestMethod.GET)
+    public String login(){
+        return ""
+    }
+
+
+}
